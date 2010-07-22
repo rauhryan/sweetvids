8f368cd7fca52d3faca4bde78826db2f3146f1ac
Ryan Rauh <rauh.ryan@ninecollective.com>
2010-07-22 02:00:16 -0500

    missed a file

---
+++ src/SweetVids.Web/Actions/Rss/RssFeed.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace SweetVids.Web.Actions.Rss
+{
+    [XmlRoot("rss")]
+    public class RssFeed
+    {
+        [XmlElement("channel")]
+        public RssChannel Channel;
+        [XmlAttribute("version")]
+        public string Version;
+        [XmlNamespaceDeclarations()]
+        public XmlSerializerNamespaces Xmlns;
+    }
+
+    public class RssChannel
+    {
+        [XmlElement("title")]
+        public string Title;
+        [XmlElement("link")]
+        public string Link;
+        [XmlElement("description")]
+        public string Description;
+        [XmlElement("language")]
+        public string Language;
+        [XmlElement("copyright")]
+        public string Copyright;
+        [XmlElement("image")]
+        public RssImage Image;
+
+        [XmlElement("item")]
+        public RssItem[] Items;
+    }
+
+    public class RssItem
+    {
+        [XmlElement("title")]
+        public string Title;
+        [XmlElement("guid")]
+        public RssGuid Guid;
+        [XmlElement("pubDate")]
+        public string PublishDate;
+        [XmlElement("description")]
+        public string Description;
+        [XmlElement("link")]
+        public string Link;
+    }
+
+    public class RssImage
+    {
+        [XmlElement("url")]
+        public string Url;
+        [XmlElement("title")]
+        public string Title;
+        [XmlElement("link")]
+        public string Link;
+    }
+
+    public class RssGuid
+    {
+        [XmlText]
+        public Guid Value;
+        [XmlAttribute("isPermaLink")]
+        public bool IsPermaLink;
+    }
+}
